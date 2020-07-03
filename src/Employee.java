c4803b306dd9c70e7d9b838d1eb4bb6630189059
Alina_D <alinadorozhkina91@mail.ru>
2020-07-03 11:52:23 +0300

    homework 5 is fixed.

--- src/Employee.java
+++ src/Employee.java
@@ -11,15 +11,50 @@ public class Employee {
         this.position=position;
         this.email=email;
         this.telephoneNumber=telephoneNumber;
-        this.salary=salary;
-        this.age=age;
+        this.salary=checkMinSalary(salary);
+        this.age=checkAge(age);
     }
-    int getAge (){
+    public void setSalary(double salary){
+        checkMinSalary(salary);
+        this.salary=salary;
+    }
+    public double getSalary (){
+        return salary;
+    }
+
+    private double checkMinSalary(double salary){
+        if (salary>12130){
+            return salary;
+
+        } else {
+            System.out.println((String.format("Warning! %s Salary %.2f is less than min. Fix it.", fullName, salary)));
+
+        }
+        return 0.00;
+
+    }
+
+    private int checkAge(int age){
+        if (age>=18){
+            return age;
+        } else {
+            System.out.println(String.format("Warning! Impossible to hire. %s younger than 18. Check age.",fullName));
+        }
+        return 0;
+
+    }
+    public void setAge(int age){
+        checkAge(age);
+        this.age=age;
+
+    }
+
+    public int getAge (){
         return age;
     }
 
-    void printEmployeeInfo(){
-        System.out.println(String.format("Employee's information:\nFullname: %s, position: %s, email: %s, telephone number: %s, salary: %.2f rubles, age: %d years", fullName,position, email,telephoneNumber,salary,age ));
+    public void printEmployeeInfo(){
+        System.out.println(String.format("Employee's information:\nFull Name: %s, position: %s, email: %s, telephone number: %s, salary: %.2f rubles, age: %d years", fullName,position, email,telephoneNumber,salary,age ));
 
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
+
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
         employees[0] = new Employee("Abramov Evgenii Ivanovich", "director", "director@mail.ru", "+79105555555", 100000.5, 50);
-        employees[1] = new Employee("Egorova Lidiia Mikhailovna", "accountant", "account@mail.ru", "+79104444444", 70000.0, 42);
+        employees[1] = new Employee("Egorova Lidiia Mikhailovna", "accountant", "account@mail.ru", "+79104444444", 10000, 42);
         employees[2] = new Employee("Ivanova Mariia Sergeevna", "administrator", "admin@mail.ru", "+79101111111", 50000.65, 32);
-        employees[3] = new Employee("Matrosova Irina Leonidovna", "waiter", "waiter@mail.ru", "+7966666666", 40000.0, 25);
+        employees[3] = new Employee("Matrosova Irina Leonidovna", "waiter", "waiter@mail.ru", "+7966666666", 40000.0, 5);
         employees[4] = new Employee("Rusakov Dmitrii Nikolaevich", "chef", "chef@mail.ru", "+7999999999", 90000.8, 41);
+
+        employees[1].setSalary(50000);
+        employees[3].setAge(50);
 
         findOlderThan40(employees);
     }
 
-    static void findOlderThan40(Employee[] employees){
+    public static void findOlderThan40(Employee[] employees){
         System.out.println("Selection by age (more than 40 years old)");
         for (int i=0;i<employees.length;i++){
             if(employees[i].getAge()>40){
